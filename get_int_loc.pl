8211021db6a4bcf39337509137f77f45c12106a1
Gabe Starrett <gjstarrett@gmail.com>
2019-01-11 08:49:11 -0500

    Bloom filter
    Addition of a bloom filter to capture reads with viral kmers without any viral alignment. Other small changes for timestamping, etc.

--- get_int_loc.pl
+++ get_int_loc.pl
@@ -1,8 +1,14 @@
 #!/usr/bin/perl -w
 use strict;
+use Bio::SeqIO;
+use Algorithm::BloomFilter;
+use Bio::Perl;
 
 my $in = shift;
 my $virus = shift;
+my $ref = shift;
+my $k = 31;
+my $timestamp = time();
 chomp($virus);
 my %intHash;
 # my %virHash;
@@ -10,37 +16,81 @@ my $threshold = 0.05; # minimum fraction of one-end, virus-mapped pairs needed f
 my $readLen = 100; # arbitrary value for viral read length
 my $totalPairs = 0;
 open(SAM, "samtools view $in |"); # read in bam/sam
-open(OUT, "> $in.$virus.bedpe");
-open(FQ, "> $in.$virus.fastq");
+open(OUT, "> $in.$virus.$timestamp.bedpe");
+open(FQ, "> $in.$virus.$timestamp.fastq");
 
+# read in fasta files
+print STDERR "Reading in reference fasta(s)\n";
+my @refFiles = split(",", $ref);
+my %seqs;
+for my $file (@refFiles) {
+  my $seqin = Bio::SeqIO->new(-format => 'fasta', -file => $file);
+  while( my $seq = $seqin->next_seq() ) {
+    $seqs{$seq->display_id()} = $seq->seq(); # append the seqdata
+  }
+}
+
+# make virus kmer bloom filter
+print STDERR "Finding unique kmers in $virus and making Bloom filter\n";
+my %seen;
+my $filter = Algorithm::BloomFilter->new(1000000, 129);
+#print "$seqs{$virus}\n";
+#exit;
+my $sequence = $seqs{$virus};
+my $revsequence = revcom($seqs{$virus})->seq();
+for my $i (0 .. length($seqs{$virus}) - $k) {
+  my $kmer = substr($sequence, $i, $k);
+  my $revKmer = substr($revsequence, $i, $k);
+  $filter->add($kmer) unless $seen{$kmer}++;
+  $filter->add($revKmer) unless $seen{$revKmer}++;
+  #print "$kmer\t$revKmer\n";
+}
+
+#print serialize($filter), "\n";
+
+# process bam
+print STDERR "Processing $in for sequences matching $virus kmers\n";
 while (<SAM>) {
-  next unless $_ =~ /$virus/;
   chomp($_);
   my @f = split("\t", $_);
-  if ($f[6] eq $virus) {
-    my $alnLen = 0;
-    my $strand = "+"; # default is positive strand for read mapping
-    if ($f[1] & hex("0x10")) { # find SAM flag for reverse complemented read
-      $strand = "-"; # set as reverse strand
+  if ($_ =~ /$virus/) {
+    if ($f[6] eq $virus) {
+      my $alnLen = 0;
+      my $strand = "+"; # default is positive strand for read mapping
+      if ($f[1] & hex("0x10")) { # find SAM flag for reverse complemented read
+        $strand = "-"; # set as reverse strand
+      }
+      while ($f[5] =~ /(\d+)([MIDNSHPX=])/g) { # functional mapping length relative to reference is calculated by cigar string
+        my $alpha = $2;
+        my $num = $1;
+        if ($alpha =~ /[MDN=X]/) {
+            $alnLen += $num;
+        }
+      }
+      push( @{$intHash{$f[2]}}, [$f[3],$f[3]+$alnLen, $f[7]]);
+      print OUT join("\t", @f[2,3], $f[3]+$alnLen, ".", ".", $strand, @f[6,7], $f[7]+$readLen, ".", ".", "."), "\n"; # Not actually bedpe format, but structured to work with bedtools
+      $totalPairs++; # counting only host read with viral mate pair
     }
-    while ($f[5] =~ /(\d+)([MIDNSHPX=])/g) { # functional mapping length relative to reference is calculated by cigar string
-      my $alpha = $2;
-      my $num = $1;
-      if ($alpha =~ /[MDN=X]/) {
-          $alnLen += $num;
+    print FQ "@" . $f[0] . "-" . $f[2] . ":" . $f[3] . "\n" . $f[9] . "\n+\n" . $f[10] . "\n"; # write out all reads in pairs containing viral sequence
+  } else {
+    for my $i (0 .. length($f[9]) - $k) {
+      my $kmer = substr($f[9], $i, $k);
+      if ($filter->test($kmer)) {
+        $totalPairs++; # counting because it is likely a read spanning the junction
+        print join("\t", @f[2,3], $f[3], ".", ".", "+", @f[6,7], $f[7]+$readLen, ".", ".", "."), "\n"; # Not actually bedpe format, but structured to work with bedtools
+        print FQ "@" . $f[0] . "-" . $f[2] . ":" . $f[3] . "\n" . $f[9] . "\n+\n" . $f[10] . "\n"; # write out all reads in pairs containing viral sequence
+        last;
+      } else {
+        next;
       }
     }
-    push( @{$intHash{$f[2]}}, [$f[3],$f[3]+$alnLen, $f[7]]);
-    print OUT join("\t", @f[2,3], $f[3]+$alnLen, ".", ".", $strand, @f[6,7], $f[7]+$readLen, ".", ".", "."), "\n"; # Not actually bedpe format, but structured to work with bedtools
-    $totalPairs++; # counting only host read with viral mate pair
   }
-  print FQ "@" . $f[0] . "-" . $f[2] . ":" . $f[3] . "\n" . $f[9] . "\n+\n" . $f[10] . "\n"; # write out all reads in pairs containing viral sequence
 }
 if ($totalPairs == 0) {
   print "No discordant pairs containing $virus found!\n\n";
   exit 0;
 }
-open(SUM, "> $in.$virus.int.txt");
+open(SUM, "> $in.$virus.$timestamp.int.txt");
 print SUM join("\t","#hostChrom","hostStart","hostEnd","virChrom","virStart","virEnd","numPairs","hostSkew", "virSkew"),"\n";
 
 # for my $key (sort keys %intHash) {
@@ -56,7 +106,7 @@ close(SAM);
 close(OUT);
 # close(FQ);
 
-open(MERGE, "bedtools merge -s -i $in.$virus.bedpe -c 7,8,9 -o count,min,max |");
+open(MERGE, "bedtools merge -s -i $in.$virus.$timestamp.bedpe -c 7,8,9 -o count,min,max |");
 while (<MERGE>) {
   chomp($_);
   my @f = split("\t",$_);
@@ -94,9 +144,13 @@ while (<MERGE>) {
 }
 close(SUM);
 
-my $spades_out = $in . "_spades";
+my $spades_out = $in . "_$timestamp" . "_spades";
 my $spades_opt = "-t 4";
-system("spades.py -s $in.$virus.fastq -o $spades_out $spades_opt");
+system("spades.py -s $in.$virus.$timestamp.fastq -o $spades_out $spades_opt");
+
+# write out saved merge fasta and create blat reference
+
+# blat contigs against human virus references
 
 # sub quartiles {
 #   my $vector = shift;
