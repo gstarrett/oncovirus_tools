99c87079eec9b29af97d259da26b25b6b3fb8285
Gabriel J Starrett <gjstarrett@gmail.com>
2022-11-29 11:17:42 -0500

    Update annotate_fastg.pl
    Correct semicolon usage in fastg files from new version of SPAdes

--- annotate_fastg.pl
+++ annotate_fastg.pl
@@ -3,7 +3,7 @@ use strict;
 use Data::Dumper;
 
 # Input blast data format
-# find /Users/starrettgj/Documents/MCC_assemblies/181024 -name "assembly_graph.fastg" | xargs -n1 -P6 -I{} blastn -query {} -out {}.blastn.txt -evalue "1e-10" -db /Volumes/data/REFS/Homo_sapiens/hg19_virus/VirusHg19_combined_Update -outfmt "6 qseqid sseqid qstart qend sstart send length mismatch gapope sstrand evalue bitscore qseq sseq"
+# -outfmt "6 qseqid sseqid qstart qend sstart send length mismatch gapope sstrand evalue bitscore qseq sseq"
 
 my $fastgData = shift;
 my $blast = shift;
@@ -13,7 +13,7 @@ open(BLAST, "< $blast");
 while (my $line = <BLAST>) {
   my @f = split("\t", $line);
   my $seqName = "";
-  if ($f[0] =~ /^([^':]+)(:.+)?;/) {
+  if ($f[0] =~ /^([^':]+)(:.+)?;?$/) {
     $seqName = $1;
     my $data = [$seqName, @f[2,3], "$f[1]:$f[4]-$f[5]"];
     push(@{$blastAnn{$seqName}}, $data);
